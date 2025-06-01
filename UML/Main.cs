11b56cf458920975afccbc3786c2c6656e5cff06
zaiet <zaiet@VLAD>
2025-06-01 09:19:31 +0300

    Add project files.

---
+++ UML/Main.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+public class Zamovnyk
+{
+    // Автоматичні властивості
+    public string Telefon { get; set; }
+    public string PIB { get; set; }
+    public string Email { get; set; }
+
+    // Конструктор без параметрів
+    public Zamovnyk()
+    {
+        Telefon = "";
+        PIB = "";
+        Email = "";
+    }
+
+    // Конструктор з параметрами
+    public Zamovnyk(string telefon, string pib, string email)
+    {
+        Telefon = telefon;
+        PIB = pib;
+        Email = email;
+    }
+
+    // Метод для виведення інформації
+    public void ShowInfo()
+    {
+        Console.WriteLine($"ПІБ: {PIB}");
+        Console.WriteLine($"Телефон: {Telefon}");
+        Console.WriteLine($"Email: {Email}");
+    }
+    public class MyString
+    {
+        private string value;
+
+        // Конструктор
+        public MyString(string val = "")
+        {
+            value = val;
+        }
+
+        // Копіювальний конструктор
+        public MyString(MyString other)
+        {
+            value = other.value;
+        }
+
+        // Присвоєння
+        public void Assign(MyString other)
+        {
+            value = other.value;
+        }
+
+        // Перевантаження оператора +
+        public static MyString operator +(MyString a, MyString b)
+        {
+            return new MyString(a.value + b.value);
+        }
+
+        // Імітація оператора <<
+        public void WriteToConsole()
+        {
+            Console.WriteLine(value);
+        }
+
+        // Імітація оператора >> (зчитування з консолі)
+        public void ReadFromConsole() => value = Console.ReadLine();
+
+        // Перевизначення ToString()
+        public override string ToString()
+        {
+            return value;
+        }
+    }
+}
+
---
+++ UML/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Zamovnyk;
+
+namespace UML
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Створення через конструктор з параметрами
+            Zamovnyk z1 = new Zamovnyk("0987654321", "Іваненко Іван Іванович", "ivan@gmail.com");
+            z1.ShowInfo();
+
+            // Створення через властивості
+            Zamovnyk z2 = new Zamovnyk();
+            z2.PIB = "Петренко Петро Петрович";
+            z2.Telefon = "0501234567";
+            z2.Email = "petro@gmail.com";
+            z2.ShowInfo();
+
+            MyString str1 = new MyString();
+            Console.Write("Введіть рядок: ");
+            str1.ReadFromConsole();  // <<
+
+            MyString str2 = new MyString(" + Додано текст");
+            MyString result = str1 + str2;  // +
+
+            Console.Write("Результат: ");
+            result.WriteToConsole();  // >>
+
+            // Присвоєння
+            MyString copy = new MyString();
+            copy.Assign(result);
+
+            Console.Write("Копія: ");
+            copy.WriteToConsole();
+
+
+        }
+
+
+    }
+
+}
